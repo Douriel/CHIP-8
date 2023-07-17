3f7600abd84e63ecea851b9ef06600cfdd8109a2
Luis Llamazares <42608443+Douriel@users.noreply.github.com>
2023-07-17 12:11:11 +0200

    Update Decode function chip8 class

--- Chip-8/Chip-8/Chip8.cpp
+++ Chip-8/Chip-8/Chip8.cpp
@@ -16,7 +16,7 @@ void Chip8::init(string gameName, int width, int height) {
 
 	std::ifstream stream("games/" + gameName, ios::in | ios::binary);
 	if(!stream.is_open()){ 
-		cout << "File not found";
+		cout << "File not found" << endl;
 	}
 	std::vector<unsigned char> contents((std::istreambuf_iterator<char>(stream)), std::istreambuf_iterator<char>());
 
@@ -85,37 +85,100 @@ void Chip8::decodeNext() {
 		switch (fouthDigit)
 		{
 		case 0:
+			m_cpu->LD_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 1:
+			m_cpu->OR_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 2:
+			m_cpu->AND_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 3:
+			m_cpu->XOR_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 4:
+			m_cpu->ADD_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 5:
+			m_cpu->SUB_Vx_Vy(secondDigit, thirdDigit);
 			break;
 		case 6:
+			m_cpu->SHR_Vx(secondDigit);
 			break;
 		case 7:
+			m_cpu->SUBN_Vx_Vy(secondDigit, thirdDigit);
 			break;
-		case 14:
+		case 0xE: // Test with 14 if not working with 'E';
 			break;
 		default:
-			cout << "Error: 8xy? instruction not found";
+			cout << "Error: 8xy? instruction not found" << endl;
 			break;
 		}
 		break;
 	case 0x9:
+		m_cpu->SNE_Vx_Vy(secondDigit, thirdDigit);
 		break;
 	case 0xA:
+		m_cpu->LD_I_addr(secondAndThirdAndFourthDigit);
 		break;
 	case 0xB:
+		m_cpu->JP_V0_addr(secondAndThirdAndFourthDigit);
 		break;
 	case 0xC:
+		m_cpu->RND_Vx_byte(secondDigit, thirdAndFourthDigit);
 		break;
+	case 0xD:
+		cout << "0xD not implemented" << endl;
+		break;
+	case 0xE:
+		switch (thirdAndFourthDigit)
+		{
+		case 0x9E:
+			cout << "0xEx9E not implemented" << endl;
+			break;
+		case 0xA1:
+			cout << "0xExA1 not implemented" << endl;
+			break;
+		default:
+			cout << "0xE??? unkown." << endl;
+			break;
+		}
+		break;
+	case 0xF:
+		switch (thirdAndFourthDigit)
+		{
+		case 0x07:
+			m_cpu->LD_Vx_DT(secondDigit);
+			break;
+		case 0x0A:
+			cout << "0xFx0ANot implemented" << endl;
+			break;
+		case 0x15:
+			cout << "0xFx15 Not implemented" << endl;
+			break;
+		case 0x18:
+			
+			break;
+		case 0x1E:
+			break;
+		case 0x29:
+			break;
+		case 0x33:
+			break;
+		case 0x55:
+			break;
+		case 0x65:
+			break;
+		default:
+			cout << "0xF??? unkown." << endl;
+			break;
+		}
+		break;
+
+
+
 	default:
+		cout << "Instruction not implemented" << endl;
 		break;
 	}
 }
